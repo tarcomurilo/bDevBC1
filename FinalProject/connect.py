4e976a47b69161b29702362ff8129b24a794fbd8
Tarço Murilo <tarcomurilo@gmail.com>
2021-10-25 12:47:02 -0700

    2510last

--- FinalProject/connect.py
+++ FinalProject/connect.py
@@ -3,10 +3,11 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from flask import Flask
+import requests
 from flask import request as flaskreq
 from sqlalchemy.sql.expression import column
 from werkzeug.utils import redirect
-from werkzeug.wrappers import request
+from bs4 import BeautifulSoup as htmlparser
 import feedparser as feed
 import datetime
 
@@ -18,6 +19,7 @@ decBase = declarative_base()
 
 # engine to connect to the file (echo to see the string parsing)
 
+
 class News(decBase):  # class to create a table
     __tablename__ = 'news'
 
@@ -26,6 +28,7 @@ class News(decBase):  # class to create a table
     summary = Column('summary', String)
     clicks = Column('clicks', Integer)
     url = Column('url', String, unique=True)
+
 
 def addNewsRow(id, title, summary, url):
     news = News()
@@ -36,9 +39,11 @@ def addNewsRow(id, title, summary, url):
     news.url = url
     return news
 
+
 eng = sqla.create_engine('sqlite:///c:\\sqlite\\database.db', echo=False)
 decBase.metadata.create_all(bind=eng)  # bind the base to the engine
 Session = sessionmaker(bind=eng)
+
 
 @app.route("/")
 def aggregator():
@@ -46,7 +51,7 @@ def aggregator():
     def catchRSS(feedUrl):
         feeding = feed.parse(feedUrl)
         articles = feeding['entries']
-        
+
         for article in articles:
             session = Session()
 
@@ -67,46 +72,87 @@ def aggregator():
 
             session.close()
 
+    def catchHTML(HTMLURL):
+        content = requests.get(HTMLURL)
+
+        content = htmlparser(content.text, 'html.parser')
+
+        allH3 = content.find_all('h3')
+
+        for title in allH3:
+
+            link = title.find_previous('a')
+            print(link.get('href'), ' - ', title.text)
+
+            # try
+            session = Session()
+
+            news = session.query(News).all()
+            lastId = -1
+            for id in news:
+                if id.id >= lastId:
+                    lastId = id.id + 1
+
+            inTitle = title.text
+            inSum = title.text
+            inUrl = link.get('href')
+            try:
+                session.add(addNewsRow(lastId, inTitle, inSum, inUrl))
+                session.commit()
+            except:
+                pass
+
+            session.close()
+            # end try
+
     FEEDURL = "http://rss.uol.com.br/feed/tecnologia.xml"
-    catchRSS(FEEDURL)  
+    # catchRSS(FEEDURL)
     FEEDURL = "https://www.inovacaotecnologica.com.br/boletim/rss.xml"
     catchRSS(FEEDURL)
     #FEEDURL = "https://www.theverge.com/rss/index.xml"
-    #catchRSS(FEEDURL)
+    # catchRSS(FEEDURL)
     #FEEDURL = "https://techcrunch.com/feed/"
-    #catchRSS(FEEDURL)
+    # catchRSS(FEEDURL)
     #FEEDURL = "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"
-    #catchRSS(FEEDURL)
+    # catchRSS(FEEDURL)
     #FEEDURL = "https://mashable.com/feeds/rss/all"
-    #catchRSS(FEEDURL) 
+    # catchRSS(FEEDURL)
+    HTMLURL = 'https://www.uol.com.br/tilt/'
+    catchHTML(HTMLURL)
 
     session = Session()
 
     news = session.query(News).all()
-    
+
     retString = '<html><style>img { max-width: 200px; height:auto}</style></html<body>'
-    retString = retString + '<h1>Latest news - Tarço aggregator - '+ format(datetime.datetime.now(),'%Y-%m-%d %H:%M:%S')+'</h1>'
+    retString = retString + '<h1>Latest news - Tarço aggregator - ' + \
+        format(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S')+'</h1>'
 
     for titles in news:
-        retString = retString + '<p><hr>Title: ' + titles.title +  '<br><br>Summary: ' +  titles.summary + '</p><br>'
-        retString = retString + '<p>Address: <a href="click?addr=' + titles.url + '">'+titles.url+'"</a> - Clicks: '+str(titles.clicks)+'</p><p></p><br><br><br>'
-    
+        retString = retString + '<p><hr>Title: ' + titles.title + \
+            '<br><br>Summary: ' + titles.summary + '</p><br>'
+        retString = retString + '<p>Address: <a href="click?addr=' + titles.url + \
+            '">'+titles.url+'"</a> - Clicks: ' + \
+            str(titles.clicks)+'</p><p></p><br><br><br>'
+
     retString = retString + '</body></html>'
     session.close()
 
     return retString
-    
+
+
 @app.route('/click', methods=['GET'])
 def clickCounter():
 
     session = Session()
-    entrie = session.query(News).filter_by(url=flaskreq.args.get('addr')).first()
-    
+    entrie = session.query(News).filter_by(
+        url=flaskreq.args.get('addr')).first()
+
     try:
         entrie.clicks = entrie.clicks + 1
         retString = entrie.url
         session.commit()
-              
+
     except:
         retString = flaskreq.args.get('addr')
         print(retString)
@@ -114,52 +160,6 @@ def clickCounter():
     session.close()
     return redirect(retString)
 
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=port)
-
-# gui
-its_on = True
-
-while its_on:
-    if its_on == False:
-        exit()
-
-    print('Type a number of command:')
-    print('1: Input data in news table')
-    print('2: consult the table')
-    print('0: to exit the program')
-
-    intext = input('> ')
-
-    if intext == '0':
-        its_on == False
-        session.close()
-        exit()
-
-    elif intext == '1':
-        session = Session()
-        news = session.query(News).all()
-        lastId = 0
-
-        for id in news:
-            if id.id >= lastId:
-                lastId = id.id + 1
-
-        print('Put the title')
-        inTitle = input('> ')
-        print('Put the summary')
-        inSum = input('> ')
-        print('Put the url')
-        inUrl = input('> ')
-        session.add(addNewsRow(lastId, inTitle, inSum, inUrl))
-        session.commit()
-
-    elif intext == '2':
-        session = Session()
-        print('Which ID do you want to see (a integer):')
-        idIn = int(input('ID: '))
-        news = session.query(News).all()
-        for titles in news:
-            print('Titles: ', titles.title, '- Summary: ', titles.summary)
-
-    session.close()
